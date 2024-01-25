efc148eb06cd03646643139bdd44936088fea3a4
Fanilo00 <faniloandriambolanoro@gmail.com>
2024-01-25 10:51:31 +0300

    Create TerrainController.java

---
+++ src/main/java/com/example/cloud_project/controller/TerrainController.java
@@ -0,0 +1,33 @@
+package com.example.cloud_project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.sql.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.cloud_project.Models.TerrainModel;
+import com.example.cloud_project.Models.PersonneModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.cloud_project.Models.PersonneModel;
+@RestController
+public class TerrainController {
+    
+    @GetMapping("Terrain/listsTerrain")
+    public String list_terrain()
+    {
+        TerrainModel t = new TerrainModel();
+        TerrainModel list_terrain[]=t.select_terrain();
+        // Convertir la liste en format JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResult = "";
+        try {
+            jsonResult = objectMapper.writeValueAsString(list_terrain);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return jsonResult;
+    }
+}

f1811c647e483ad8b7df44bccf370d456776668b
TokyHenintsoa18 <tokyramanalina@gmail.com>
2024-01-29 12:25:35 +0300

    Update CategorieController.java

--- src/main/java/com/example/cloud_project/controller/CategorieController.java
+++ src/main/java/com/example/cloud_project/controller/CategorieController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,7 +53,7 @@ public class CategorieController {
     }
 
     @CrossOrigin(origins = "*")
-    @PostMapping("categories/update_categorie")
+    @PutMapping("categories/update_categorie")
     public ResponseEntity<CategorieModel> updateCategorie(
             @RequestParam int rendementParPieds,
             @RequestParam double prixUnitaire,
